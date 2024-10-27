ebcfa1ba927e2ad062252b67bb783afccc0576f2
Eleven728 <836195067@qq.com>
2024-10-27 08:19:38 +0800

    Update TestPush.java

--- src/main/java/TestPush.java
+++ src/main/java/TestPush.java
@@ -1,6 +1,7 @@
 public class TestPush {
     public static void main(String[] args) {
         System.out.println(1231232);
-        System.out.println("20241027��֧");
+        System.out.println("20241027·ÖÖ§");
+        System.out.println("update from github")
     }
 }
