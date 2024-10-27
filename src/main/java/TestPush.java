da41530ae1226039133c1844a14ed94e0799953a
Eleven728 <836195067@qq.com>
2024-10-27 08:32:59 +0800

    Update TestPush.java

--- src/main/java/TestPush.java
+++ src/main/java/TestPush.java
@@ -2,6 +2,6 @@ public class TestPush {
     public static void main(String[] args) {
         System.out.println(1231232);
         System.out.println("20241027最新分支");
-        System.out.println("update from github");
+        System.out.println("update from github 2");
     }
 }
